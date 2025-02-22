bf648e2bf7829ce141b0d07ec96e729e68f4918f
ElsyPriya2616 <elsypriyagthimithy2616@gmail.com>
2025-02-22 23:43:37 +0530

    Adding node

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react"
+// import ReactDOM from "react-dom"
+import { createRoot } from 'react-dom/client'
+
+const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"Iam an H1 tag"),
+        React.createElement("h2",{},"Iam an H2 tag"),
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"Iam an H1 tag"),
+        React.createElement("h2",{},"Iam an H2 tag"),
+    ])
+)
+
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(parent)
+root.render(<App />);
