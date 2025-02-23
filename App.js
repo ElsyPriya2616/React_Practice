336f6a103f54aa2af3abe08e03b72e558d7ba8fb
ElsyPriya2616 <elsypriyagthimithy2616@gmail.com>
2025-02-23 19:44:56 +0530

    Adding parcel

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import React from "react"
-// import ReactDOM from "react-dom"
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},
+const parent = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
         React.createElement("h1",{},"Iam an H1 tag"),
         React.createElement("h2",{},"Iam an H2 tag"),
@@ -11,10 +10,9 @@ const parent = React.createElement("div",{id:"parent"},
         React.createElement("h1",{},"Iam an H1 tag"),
         React.createElement("h2",{},"Iam an H2 tag"),
     ])
-)
+]);
 
 console.log(parent)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(parent)
-root.render(<App />);
+root.render(parent);
